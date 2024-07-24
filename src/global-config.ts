4bb6131bfacfb7ae2abba5fd487720558bb70e99
Erick Germani <erickgermani13@gmail.com>
2024-07-24 03:32:56 -0300

    Aplicando GlobalPipes, GlobalInterceptors e GlobalFilters

---
+++ src/global-config.ts
@@ -0,0 +1,27 @@
+import { Reflector } from '@nestjs/core';
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+import { WrapperDataInterceptor } from './shared/infrastructure/interceptors/wrapper-data/wrapper-data.interceptor';
+import { ConflictErrorFilter } from './shared/infrastructure/exception-filters/conflict-error/conflict-error.filter';
+import { NotFoundErrorFilter } from './shared/infrastructure/exception-filters/not-found-error/not-found-error.filter';
+
+export function applyGlobalConfig(app: INestApplication) {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(
+    new WrapperDataInterceptor(),
+    new ClassSerializerInterceptor(app.get(Reflector)),
+  );
+
+  app.useGlobalFilters(new ConflictErrorFilter(), new NotFoundErrorFilter());
+}
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { applyGlobalConfig } from './global-config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  applyGlobalConfig(app);
 
   await app.listen(3000, '0.0.0.0');
 }
